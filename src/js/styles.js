51a05cd2fbb18e794dff2bf9484422bf256baebd
Esteban Calvi <esteban.calvi@recargapay.com>
2019-08-08 12:01:20 -0300

    Adds variables file

--- src/js/styles.js
+++ src/js/styles.js
@@ -1,9 +1,10 @@
 import { StyleSheet } from "react-native";
+import * as Variables from '../variables/variables';
 
 export default StyleSheet.create({
   Container: {
     flex: 1,
     paddingTop: 50,
-    backgroundColor: "red"
+    backgroundColor: Variables.backgroundColor
   }
 })
